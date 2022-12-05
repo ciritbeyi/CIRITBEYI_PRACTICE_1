510d08ae2b945f70875ea4d64f8b0d93aa59d994
Ozgur Ciritbeyi <ozgrcrtby@gmail.com>
2022-12-05 21:28:14 +0100

    added more Task2_1 feature

--- src/Chapter2/Task2_1.java
+++ src/Chapter2/Task2_1.java
@@ -4,5 +4,7 @@ public class Task2_1 {
 
     public static void main(String[] args) {
         System.out.println("Hello Ediz How are you?");
+        System.out.println("Local Repo is already published to GitHub");
+        System.out.println("Doing more work");
     }
 }
