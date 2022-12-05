b957f570b8216eeae5629df62187a2e15beaf6d1
Ozgur Ciritbeyi <ozgrcrtby@gmail.com>
2022-12-05 19:56:46 +0100

    just started

---
+++ src/Chapter1/Task1_1.java
@@ -0,0 +1,7 @@
+package Chapter1;
+
+public class Task1_1 {
+    public static void main(String[] args) {
+        System.out.println("I am trying to learn Git & Github");
+    }
+}
---
+++ src/Chapter2/Task2_1.java
@@ -0,0 +1,8 @@
+package Chapter2;
+
+public class Task2_1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Ediz How are you?");
+    }
+}
